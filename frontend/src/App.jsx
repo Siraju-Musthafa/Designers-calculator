04df34481a1ddb378082de8b03e1c229cfd06b1b
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-06-14 11:39:27 +0530

    fix import case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import Calculator from "./pages/calculator";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import DesignPublish from "./pages/welcomepage";
+import WelcomePage from "./pages/welcomepage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<DesignPublish />} />
+          <Route path="/" element={<WelcomePage />} />
           <Route path="/calculator" element={<Calculator />} />
         </Routes>
       </div>
